30aa7f464df919b2725187dc23b940f7fbb34683
Faishal <faishaljaffar@gmail.com>
2025-06-23 23:05:05 +0300

    Updated api route

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,7 +1,10 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export function GET() {
-  return NextResponse.json({
-    message: "Hello, world!",
-  });
+// export function GET() {
+//   return NextResponse.json({
+//     message: "Hello, world!",
+//   });
+// }
+export async function GET(req: Request) {
+  return new Response("Hello world");
 }
--- next.config.js
+++ next.config.js
@@ -5,20 +5,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-// const nextConfig = {
-//   reactStrictMode: true,
-//   webpack: (config) => {
-//     config.resolve = {
-//       ...config.resolve,
-//       fallback: {
-//         fs: false,
-//       },
-//     };
-//     return config;
-//   },
-// };
-
-// module.exports = nextConfig;
-
-// Injected content via Sentry wizard below
